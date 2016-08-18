78c46b133a519964fb05f3c65cdc35f07b08831b
Mia <miamollie@gmail.com>
2016-08-18 11:05:16 +1000

    Fix typo

--- src/camelCaseToLowerCase.js
+++ src/camelCaseToLowerCase.js
@@ -4,4 +4,4 @@ const camelCaseToLowerCase = (string: string): string => {
   return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
 }
 
-export defaul camelCaseToLowerCase
+export default camelCaseToLowerCase
